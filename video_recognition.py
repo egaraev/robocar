68a647c424e0245e8424dffea3646ff240579cd1
Eldar <egaraev@gmail.com>
2022-02-16 22:28:00 +0100

    changed cv2 to video mode

--- video_recognition.py
+++ video_recognition.py
@@ -1,17 +1,33 @@
 import cv2
+import numpy as np
 
-camera = cv2.VideoCapture(0)
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(0)
 
-camera.set(3, 1280)
-camera.set(4, 720)
+# Check if camera opened successfully
+if (cap.isOpened()== False):
+  print("Error opening video stream or file")
 
-while True:
-    ret, image = camera.read()
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
 
-    cv2.imshow('Webcam', image)
+    # Display the resulting frame
+    cv2.imshow('Frame',frame)
 
-    if cv2.waitKey(0) & 0xFF == ord('q'):
-        break
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
 
-camera.release()
+  # Break the loop
+  else:
+    break
+
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
 cv2.destroyAllWindows()
