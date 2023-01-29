dd9ef4c08fe772d6eb9d6154a289849d019917fc
Eldar <egaraev@gmail.com>
2023-01-29 13:53:37 +0100

    Added lane detection module

--- motortest.py
+++ motortest.py
@@ -1,22 +1,55 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BCM)
+leftForward = 2
+leftBackward = 3
+leftEnable = 4
+
+rightForward = 17
+rightBackward = 22
+rightEnable = 27
+
 GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-Ena = 2
-In1 = 3
-In2 = 4
-GPIO.setup(Ena, GPIO.OUT)
-GPIO.setup(In1, GPIO.OUT)
-GPIO.setup(In2, GPIO.OUT)
-pwmA = GPIO.PWM(Ena, 100);
-pwmA.start(0);
-GPIO.cleanup()
+GPIO.setup(leftForward, GPIO.OUT)
+GPIO.setup(leftBackward, GPIO.OUT)
+GPIO.setup(leftEnable, GPIO.OUT)
+GPIO.setup(rightForward, GPIO.OUT)
+GPIO.setup(rightBackward, GPIO.OUT)
+GPIO.setup(rightEnable, GPIO.OUT)
 
-pwmA.ChangeDutyCycle(50);
-GPIO.output(In1,GPIO.LOW)
-GPIO.output(In2,GPIO.HIGH)
-sleep(2)
-pwmA.ChangeDutyCycle(0);
-GPIO.cleanup()
+leftEnPWM = GPIO.PWM(leftEnable, 100)
+leftEnPWM.start(50)
+GPIO.output(leftEnable, GPIO.HIGH)
+
+rightEnPWM = GPIO.PWM(rightEnable, 100)
+rightEnPWM.start(50)
+GPIO.output(rightEnable, GPIO.HIGH)
+
+while True:
+    GPIO.output(leftForward, GPIO.HIGH)
+    GPIO.output(rightForward, GPIO.HIGH)
+    sleep(1)
+    leftEnPWM.ChangeDutyCycle(100)
+    rightEnPWM.ChangeDutyCycle(100)
+    sleep(1)
+    leftEnPWM.ChangeDutyCycle(50)
+    rightEnPWM.ChangeDutyCycle(50)
+    sleep(1)
+    GPIO.output(leftForward, GPIO.LOW)
+    GPIO.output(rightForward, GPIO.LOW)
+    sleep(1)
+
+    GPIO.output(leftBackward, GPIO.HIGH)
+    GPIO.output(rightBackward, GPIO.HIGH)
+    sleep(1)
+    leftEnPWM.ChangeDutyCycle(100)
+    rightEnPWM.ChangeDutyCycle(100)
+    sleep(1)
+    leftEnPWM.ChangeDutyCycle(50)
+    rightEnPWM.ChangeDutyCycle(50)
+    sleep(1)
+    GPIO.output(leftBackward, GPIO.LOW)
+    GPIO.output(rightBackward, GPIO.LOW)
+    sleep(1)
