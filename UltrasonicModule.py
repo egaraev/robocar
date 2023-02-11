738e4a2277f7682705b6d4e6eddd5066a1bcbf0c
Eldar Garayev <egaraev@gmail.com>
2023-02-11 11:04:02 +0100

    added ultrasonic sensor new code

--- UltrasonicModule.py
+++ UltrasonicModule.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
-
+from MotorModule import Motor
+motor = Motor(22, 27, 17, 2, 4, 3)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -20,29 +21,49 @@ print ("Place the object......")
 
 
 try:
+    count =0
     while True:
-       GPIO.output(TRIG, True)
-       time.sleep(0.00001)
-       GPIO.output(TRIG, False)
+        i =0
+        avgDistance =0
+        for i in range (5):
+            GPIO.output(TRIG, False)
+            time.sleep(0.1)
+            GPIO.output(TRIG, True)
+            time.sleep(0.00001)
+            GPIO.output(TRIG, False)
 
-       while GPIO.input(ECHO)==0:
-          pulse_start = time.time()
+        while GPIO.input(ECHO)==0:
+            pulse_start = time.time()
 
-       while GPIO.input(ECHO)==1:
-          pulse_end = time.time()
+        while GPIO.input(ECHO)==1:
+            pulse_end = time.time()
+            pulse_duration = pulse_end - pulse_start
 
-       pulse_duration = pulse_end - pulse_start
-       distance = pulse_duration * 17150
-       distance = round(distance+1.15, 2)
-
-       if distance<=20 and distance>=5:
-          print ("distance:",distance,"cm")
-          i=1
-
-       if distance>20 and i==1:
-          print ("place the object....")
-          i=0
-       time.sleep(2)
+            distance = pulse_duration * 17150
+            distance = round(distance, 2)
+            avgDistance = avgDistance+distance
+            avgDistance=avgDistance/5
+            print(avgDistance)
+        flag =0
+        if avgDistance<20:
+            print ("distance:",distance,"cm")
+            count = count+1
+            motor.stop()
+            time.sleep(1)
+            motor.backward(0.3, 1)
+            time.sleep(1.5)
+            if count%3==1&(flag==0):
+                motor.move(0, -0.7, 1)
+                flag=1
+            else:
+                motor.move(0, 0.7, 1)
+                flag =0
+                time.sleep(1.5)
+                motor.stop()
+                time.sleep(1)
+        else:
+            motor.move(0.3, 0.0, 1)
+            flag =0
 
 except KeyboardInterrupt:
-     GPIO.cleanup()
+        GPIO.cleanup()
