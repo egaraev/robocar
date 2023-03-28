f60c33fd6b7450fdc16df32af5609c840beb386d
Eldar <egaraev@gmail.com>
2023-03-28 20:56:31 +0200

    Added Ml lane follow

---
+++ Tempfiles/ML_lane_follow/JoystickModule.py
@@ -0,0 +1,131 @@
+# Load library functions we want
+import time
+import pygame
+import os
+import sys
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+from MotorModule import Motor
+motor = Motor(22, 27, 17, 2, 4, 3)
+
+axisUpDown = 1                          # Joystick axis to read for up / down position
+axisUpDownInverted = False              # Set this to True if up and down appear to be swapped
+axisLeftRight = 3                       # Joystick axis to read for left / right position
+axisLeftRightInverted = False           # Set this to True if left and right appear to be swapped
+interval = 0.1                          # Time between keyboard updates in seconds, smaller responds faster but uses more processor time
+
+# Setup pygame and key states
+global hadEvent
+global moveUp
+global moveDown
+global moveLeft
+global moveRight
+global moveQuit
+hadEvent = True
+moveUp = False
+moveDown = False
+moveLeft = False
+moveRight = False
+moveQuit = False
+pygame.init()
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+
+# Function to handle pygame events
+def PygameHandler(events):
+    # Variables accessible outside this function
+    global hadEvent
+    global moveUp
+    global moveDown
+    global moveLeft
+    global moveRight
+    global moveQuit
+
+    # Handle each event individually
+    for event in events:
+        if event.type == pygame.QUIT:
+            # User exit
+            hadEvent = True
+            moveQuit = True
+        elif event.type == pygame.KEYDOWN:
+            # A key has been pressed, see if it is one we want
+            hadEvent = True
+            if event.key == pygame.K_ESCAPE:
+                moveQuit = True
+        elif event.type == pygame.KEYUP:
+            # A key has been released, see if it is one we want
+            hadEvent = True
+            if event.key == pygame.K_ESCAPE:
+                moveQuit = False
+        elif event.type == pygame.JOYAXISMOTION:
+            # A joystick has been moved, read axis positions (-1 to +1)
+            hadEvent = True
+            upDown = joystick.get_axis(axisUpDown)
+            leftRight = joystick.get_axis(axisLeftRight)
+            # Invert any axes which are incorrect
+            if axisUpDownInverted:
+                upDown = -upDown
+            if axisLeftRightInverted:
+                leftRight = -leftRight
+            # Determine Up / Down values
+            if upDown < -0.1:
+                moveUp = True
+                moveDown = False
+
+
+            elif upDown > 0.1:
+                moveUp = False
+                moveDown = True
+
+
+            else:
+                moveUp = False
+                moveDown = False
+            # Determine Left / Right values
+            if leftRight < -0.1:
+                moveLeft = True
+                moveRight = False
+
+
+            elif leftRight > 0.1:
+                moveLeft = False
+                moveRight = True
+
+
+            else:
+                moveLeft = False
+                moveRight = False
+
+
+try:
+    print ('Press [ESC] or Press PS3 O button to quit')
+    # Loop indefinitely
+    while True:
+        # Get the currently pressed keys on the keyboard
+        PygameHandler(pygame.event.get())
+        if hadEvent:
+            # Keys have changed, generate the command list based on keys
+            hadEvent = False
+            if moveQuit:
+                break
+            elif moveLeft:
+                print ("Moving left")
+                motor.move(0, 0.9, 0.1)
+            elif moveRight:
+                print ("Moving right")
+                motor.move(0, -0.9, 0.1)
+            elif moveUp:
+                print ("Moving forward")
+                motor.move(0.7, 0.0, 0.1)
+            elif moveDown:
+                print ("Moving backward")
+                motor.backward(0.7, 0.1)
+            else:
+                print ("Slowing down")
+                motor.stop()
+        # Wait for the interval period
+        time.sleep(interval)
+except KeyboardInterrupt:
+    # CTRL+C exit, disable all drives
+    motor.stop(5)
--- Tempfiles/test_lane_follow.py
+++ Tempfiles/test_lane_follow.py
@@ -1,58 +1,17 @@
 import cv2
 import numpy as np
-import math
-import logging
-_SHOW_IMAGE = False
-#frame = cv2.imread('Pibot.JPG')
+import matplotlib.pyplot as plt
 
-class HandCodedLaneFollower(object):
-
-    def __init__(self, car=None):
-        logging.info('Creating a HandCodedLaneFollower...')
-        self.car = car
-        self.curr_steering_angle = 90
-
-    def follow_lane(self, frame):
-        # Main entry point of the lane follower
-        show_image("orig", frame)
-
-        lane_lines, frame = detect_lane(frame)
-        final_frame = self.steer(frame, lane_lines)
-
-        return final_frame
-
-    def steer(self, frame, lane_lines):
-        logging.debug('steering...')
-        if len(lane_lines) == 0:
-            logging.error('No lane lines detected, nothing to do.')
-            return frame
-
-        new_steering_angle = compute_steering_angle(frame, lane_lines)
-        self.curr_steering_angle = stabilize_steering_angle(self.curr_steering_angle, new_steering_angle, len(lane_lines))
-
-        if self.car is not None:
-            self.car.front_wheels.turn(self.curr_steering_angle)
-        curr_heading_image = display_heading_line(frame, self.curr_steering_angle)
-        show_image("heading", curr_heading_image)
-
-        return curr_heading_image
-
-
-def show_image(title, frame, show=_SHOW_IMAGE):
-    if show:
-        cv2.imshow(title, frame)
-
-
-def make_points(frame, line):
+def make_points(image, line):
     slope, intercept = line
-    y1 = int(frame.shape[0])  # bottom of the image
+    y1 = int(image.shape[0])  # bottom of the image
     y2 = int(y1 * 3 / 5)  # slightly lower than the middle
     x1 = int((y1 - intercept) / slope)
     x2 = int((y2 - intercept) / slope)
     return np.array([x1, y1, x2, y2])
 
 
-def average_slope_intercept(frame, lines):
+def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
     if lines is not None:
@@ -72,21 +31,21 @@ def average_slope_intercept(frame, lines):
         # add more weight to longer lines
         left_fit_average = np.average(left_fit, axis=0)
         right_fit_average = np.average(right_fit, axis=0)
-        left_line = make_points(frame, left_fit_average)
-        right_line = make_points(frame, right_fit_average)
+        left_line = make_points(image, left_fit_average)
+        right_line = make_points(image, right_fit_average)
         averaged_lines = [left_line, right_line]
         return averaged_lines
 
-def canny(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     kernel = 5
     blur = cv2.GaussianBlur(gray, (kernel, kernel), 0)
     canny = cv2.Canny(blur, 50, 350)
     return canny
 
 
-def display_lines (frame, lines):
-    line_image=np.zeros_like(frame)
+def display_lines (image, lines):
+    line_image=np.zeros_like(image)
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
@@ -95,134 +54,123 @@ def display_lines (frame, lines):
 
 
 
-def region_of_interest(frame):
-    height = frame.shape[0]
+def region_of_interest(image):
+    height = image.shape[0]
     polygons = np.array([[
         (0, height), (950, height), (475, 200)
     ]])
-    mask = np.zeros_like(frame)
+    mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    masked_image = cv2.bitwise_and(frame, mask)
+    masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
-def compute_steering_angle(frame, lane_lines):
-    """ Find the steering angle based on lane line coordinate
-        We assume that camera is calibrated to point to dead center
-    """
-    if len(lane_lines) == 0:
-        logging.info('No lane lines detected, do nothing')
-        return -90
-
-    height, width, _ = frame.shape
-    if len(lane_lines) == 1:
-        logging.debug('Only detected one lane line, just follow it. %s' % lane_lines[0])
-        x1, _, x2, _ = lane_lines[0][0]
-        x_offset = x2 - x1
-    else:
-        _, _, left_x2, _ = lane_lines[0][0]
-        _, _, right_x2, _ = lane_lines[1][0]
-        camera_mid_offset_percent = 0.02 # 0.0 means car pointing to center, -0.03: car is centered to left, +0.03 means car pointing to right
-        mid = int(width / 2 * (1 + camera_mid_offset_percent))
-        x_offset = (left_x2 + right_x2) / 2 - mid
-
-    # find the steering angle, which is angle between navigation direction to end of center line
-    y_offset = int(height / 2)
-
-    angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
-    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
-    steering_angle = angle_to_mid_deg + 90  # this is the steering angle needed by picar front wheel
-
-    logging.debug('new steering angle: %s' % steering_angle)
-    return steering_angle
-
-def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_width=5 ):
-    heading_image = np.zeros_like(frame)
-    height, width, _ = frame.shape
-
-    # figure out the heading line from steering angle
-    # heading line (x1,y1) is always center bottom of the screen
-    # (x2, y2) requires a bit of trigonometry
-
-    # Note: the steering angle of:
-    # 0-89 degree: turn left
-    # 90 degree: going straight
-    # 91-180 degree: turn right
-    steering_angle_radian = steering_angle / 180.0 * math.pi
-    x1 = int(width / 2)
-    y1 = height
-    x2 = int(x1 - height / 2 / math.tan(steering_angle_radian))
-    y2 = int(height / 2)
-
-    cv2.line(heading_image, (x1, y1), (x2, y2), line_color, line_width)
-    heading_image = cv2.addWeighted(frame, 0.8, heading_image, 1, 1)
-
-    return heading_image
 
 
-def stabilize_steering_angle(curr_steering_angle, new_steering_angle, num_of_lane_lines,
-                             max_angle_deviation_two_lines=5, max_angle_deviation_one_lane=1):
-    """
-    Using last steering angle to stabilize the steering angle
-    This can be improved to use last N angles, etc
-    if new angle is too different from current angle, only turn by max_angle_deviation degrees
-    """
-    if num_of_lane_lines == 2:
-        # if both lane lines detected, then we can deviate more
-        max_angle_deviation = max_angle_deviation_two_lines
-    else:
-        # if only one lane detected, don't deviate too much
-        max_angle_deviation = max_angle_deviation_one_lane
-
-    angle_deviation = new_steering_angle - curr_steering_angle
-    if abs(angle_deviation) > max_angle_deviation:
-        stabilized_steering_angle = int(curr_steering_angle
-                                        + max_angle_deviation * angle_deviation / abs(angle_deviation))
-    else:
-        stabilized_steering_angle = new_steering_angle
-    logging.info('Proposed angle: %s, stabilized angle: %s' % (new_steering_angle, stabilized_steering_angle))
-    return stabilized_steering_angle
+image = cv2.imread('Pibot.JPG')
+lane_image = np.copy(image)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=7)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow("result", combo_image)
+cv2.waitKey(0)
 
 
-def detect_lane(frame):
-    logging.debug('detecting lane lines...')
-    lane_image = np.copy(frame)
-    canny_image = canny(lane_image)
-    cropped_image = region_of_interest(canny_image)
-    lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=7)
-    averaged_lines = average_slope_intercept(lane_image, lines)
-    lane_lines = averaged_lines
-    line_image = display_lines(lane_image, averaged_lines)
-    combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-    lane_lines_image = combo_image
-    show_image("lane lines", combo_image)
-    cv2.waitKey(0)
+# def make_points(image, line):
+#     slope, intercept = line
+#     y1 = int(image.shape[0])  # bottom of the image
+#     y2 = int(y1 * 3 / 5)  # slightly lower than the middle
+#     x1 = int((y1 - intercept) / slope)
+#     x2 = int((y2 - intercept) / slope)
+#     return [[x1, y1, x2, y2]]
+#
+#
+# def average_slope_intercept(image, lines):
+#     left_fit = []
+#     right_fit = []
+#     if lines is None:
+#         return None
+#     for line in lines:
+#         for x1, y1, x2, y2 in line:
+#             fit = np.polyfit((x1, x2), (y1, y2), 1)
+#             slope = fit[0]
+#             intercept = fit[1]
+#             if slope < 0:  # y is reversed in image
+#                 left_fit.append((slope, intercept))
+#             else:
+#                 right_fit.append((slope, intercept))
+#     # add more weight to longer lines
+#     left_fit_average = np.average(left_fit, axis=0)
+#     right_fit_average = np.average(right_fit, axis=0)
+#     left_line = make_points(image, left_fit_average)
+#     right_line = make_points(image, right_fit_average)
+#     averaged_lines = [left_line, right_line]
+#     return averaged_lines
+#
+#
+# def canny(img):
+#     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+#     kernel = 5
+#     blur = cv2.GaussianBlur(gray, (kernel, kernel), 0)
+#     canny = cv2.Canny(gray, 50, 150)
+#     return canny
+#
+#
+# def display_lines(img, lines):
+#     line_image = np.zeros_like(img)
+#     if lines is not None:
+#         for line in lines:
+#             for x1, y1, x2, y2 in line:
+#                 cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+#     return line_image
+#
+#
+# def region_of_interest(canny):
+#     height = canny.shape[0]
+#     width = canny.shape[1]
+#     mask = np.zeros_like(canny)
+#
+#     triangle = np.array([[
+#         (200, height),
+#         (550, 250),
+#         (1100, height), ]], np.int32)
+#
+#     cv2.fillPoly(mask, triangle, 255)
+#     masked_image = cv2.bitwise_and(canny, mask)
+#     return masked_image
+#
+#
+# image = cv2.imread('test_image.jpg')
+# lane_image = np.copy(image)
+# lane_canny = canny(lane_image)
+# cropped_canny = region_of_interest(lane_canny)
+# lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+# averaged_lines = average_slope_intercept(image, lines)
+# line_image = display_lines(lane_image, averaged_lines)
+# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
+# cv2.imshow("result", combo_image)
+# cv2.waitKey(0)  # Wait indefinitely for a key press
+# cv2.destroyAllWindows()  # Close all OpenCV windows when a key is pressed
 
-    return lane_lines, lane_lines_image
 
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-
-    # Initialize the camera
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        logging.error("Error opening video stream or file")
-        exit()
-
-    lane_follower = HandCodedLaneFollower()
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            final_frame = lane_follower.follow_lane(frame)
-            cv2.imshow("Result", final_frame)
-
-            # Press 'q' to quit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-        else:
-            break
-
-    # Release the video capture object and close windows
-    cap.release()
-    cv2.destroyAllWindows()
+#
+#cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture("test2.mp4")
+# while(cap.isOpened()):
+#     ret, frame = cap.read()
+#     if ret == True:
+#         canny_image = canny(frame)
+#         cropped_canny = region_of_interest(canny_image)
+#         lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+#         averaged_lines = average_slope_intercept(frame, lines)
+#         line_image = display_lines(frame, averaged_lines)
+#         combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+#         cv2.imshow("result", combo_image)
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
+#     else:
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
